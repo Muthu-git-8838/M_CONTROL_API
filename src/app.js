f37c78b0397961be0463cefbc3172eafe2d92f43
Muthu-git-8838 <muthumani8838.elonnative@gmail.com>
2023-06-07 16:07:11 +0530

    404 commit

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "*",
   },
 });
 app.get("/", async (req, res) => {
---
+++ src/model/messageSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const messageSchema = mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "mcontrol" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    readBy: { type: mongoose.Schema.Types.ObjectId, ref: "mcontrol" },
+  },
+  { timestamps: true }
+);
+
+const Messages = mongoose.model("Message", messageSchema);
+
+module.exports = Messages;
